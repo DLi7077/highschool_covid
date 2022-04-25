4e8a187595006e6c9ce62c47a166c281f51b4873
Devin Li <51217000+butterriolu@users.noreply.github.com>
2022-04-25 17:04:58 -0400

    added intro descrip

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function App() {
   return (
     <div className= 'body'>
       <div className= 'part'>
-        <div className= "title">Intro</div>
+        <div className= "title">Highschool Education vs Covid</div>
+        <div className= "subtext">
+          This project seeks to determine Covid-19's affect on highschool education.
+        </div>
       </div>
 
       <div className= 'part'>
@@ -24,20 +27,40 @@ function App() {
               </ul>
               </div>
               <div className="textGroup">
-                Possible forms of Measurement:
-                <ul>
-                  <li>Grades/ GPA</li>
-                  <li>Attendance Rate</li>
-                  <li>Graduation Rate</li>
+              Brief Problems:
+              <ul>
+                <li>Grades/GPA not recorded</li>
+                <li>Attendance Rates for 2020 not recorded</li>
+                <li>Graduation Rates recorded annually</li>
                 </ul>
-            </div>
+              </div>
+          </div>
+
+          <div className="title">Data Collection</div>
+          <div className="subtext">
+            annual highschool attendance and graduation rates:
+            <br></br>
+            2021: https://data.cityofnewyork.us/Education/2021-DOE-High-School-Directory/8b6c-7uty
+            <br></br>
+            2020: https://data.cityofnewyork.us/Education/2020-DOE-High-School-Directory/23z9-6uk9
+            <br></br>
+            2019: https://data.cityofnewyork.us/Education/2019-DOE-High-School-Directory/uq7m-95z8
+            <br></br>
+            2018: https://data.cityofnewyork.us/Education/2018-DOE-High-School-Directory/vw9i-7mzq
+            <br></br>
+            2017: https://data.cityofnewyork.us/Education/2017-DOE-High-School-Directory/s3k6-pzi2
+            <br></br>
+            2021 remote learning data: https://infohub.nyced.org/reports/government-reports/remote-learning-attendance-data
+            <br></br>
+            Covid Data: https://data.cityofnewyork.us/Health/COVID-19-Daily-Counts-of-Cases-Hospitalizations-an/rc75-m7u3
           </div>
         <div className="group">
-        <GraphSlide graphType={'Attendance Rates'} year= {2017}/>
-        <GraphSlide graphType={'Graduation Rates'} year= {2017}/>
-        <hr></hr>
-        <GraphSlide graphType={'Correlation'} year= {2017}/>
-        <GraphSlide graphType={'PieChart'}    year= {2017}/>
+          
+          <GraphSlide graphType={'Attendance Rates'} year= {2017}/>
+          <GraphSlide graphType={'Graduation Rates'} year= {2017}/>
+          <hr></hr>
+          <GraphSlide graphType={'Correlation'} year= {2017}/>
+          <GraphSlide graphType={'PieChart'}    year= {2017}/>
         </div>
         
 
